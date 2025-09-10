82f438f4d89123a3769916c614595bbf0ffd5bcf
MarceloMoraisLeeds <morais.m@hotmail.com>
2025-09-10 01:34:28 +0100

    Adicionando funcoes e testes

---
+++ test/gerenciarUsuarios.test.js
@@ -0,0 +1,15 @@
+const { retornarUsuarios, adicionarNovoUsuario } = require('../src/gerenciarUsuarios');
+const { expect } = require('chai');
+
+describe('Testar a funcao de gestao de usuarios', function () {
+    it('Validar que posso adicionar um novo nome de usuario na lista', function () {
+        // 1. Adicionar um novo usuario na lista de usuarios
+        adicionarNovoUsuario('Maria');
+
+        // 2. Retornar a lista de usuarios da caixa de lista de usuarios
+        const listaDeUsuarios = retornarUsuarios();
+
+        // 3. Comparar se o novo usuario esta na lista de usuarios
+        expect(listaDeUsuarios.at(-1)).to.equal('Maria');
+    });
+});
